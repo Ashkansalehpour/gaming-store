3cc2db115a50fc7d631425120edc5c0561a4e228
Ashkan <ashkansalehpoor81@gmail.com>
2024-09-02 16:07:09 +0330

    feat(data): add product data and images
    Added a product data file `src/data/products.js` containing an array of product objects with details such as `id`, `name`, `price`, `dateAdded`, `category`, and `image`.
    
    Sample product data includes:
    - `Gaming Chair`
    - `Gaming Monitor`
    - `Gaming Mouse`
    - `Gaming Keyboard`
    
    added images corresponding to each product:
    - `[Razer]GamingChair.png`
    - `[Razer]GamingKeyboard.png`
    - `[Razer]GamingMouse.png`
    - `[Xiaomi]GamingMonitor.png`
    
    These additions provide the foundational data and assets needed for displaying products in the application.

---
+++ src/data/Products.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+      id: 1,
+      name: 'Gaming Chair',
+      price: 150,
+      dateAdded: '2024-08-01',
+      category: 'chairs',
+      image: '[Razer]GamingChair.png',
+    },
+    {
+      id: 2,
+      name: 'Gaming Monitor',
+      price: 300,
+      dateAdded: '2024-08-05',
+      category: 'monitors',
+      image: '[Xiaomi]GamingMonitor.png',
+    },
+    {
+      id: 3,
+      name: 'Gaming Mouse',
+      price: 50,
+      dateAdded: '2024-08-10',
+      category: 'mice',
+      image: '[Razer]GamingMouse.png',
+    },
+    {
+      id: 4,
+      name: 'Gaming Keyboard',
+      price: 100,
+      dateAdded: '2024-08-15',
+      category: 'keyboards',
+      image: 'gaming-keyboard.jpg',
+    },
+  ];
+  
+  export default products;
