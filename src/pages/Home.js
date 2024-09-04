2c350b6a3eb4e6cef3c8fb82109d37f54256c448
Ashkan <ashkansalehpoor81@gmail.com>
2024-09-04 13:49:35 +0330

    feat(pages): add Home page component
    Created a `Home` page component in `src/pages/Home.js` to serve as the landing page of the application.
    
    Features:
    - Displays a welcoming message: "Welcome to the Gaming Store."
    - Includes a brief description encouraging users to explore gaming products.
    - Basic structure to introduce users to the application.
    
    This component sets up the initial view that users see when they visit the application.

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to the Gaming Store</h1>
+      <p>Find the best gaming products here!</p>
+    </div>
+  );
+}
+
+export default Home;
