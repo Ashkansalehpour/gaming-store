5f530bc07e539e12e297a04a2901329c38926e4b
Ashkan <ashkansalehpoor81@gmail.com>
2024-09-04 15:37:27 +0330

    chore(entry-point): update index.js to apply global styles
    Updated `src/index.js` to ensure global styles are applied across the application.
    
    Changes include:
    - Imported global styles from `App.css`.
    - Wrapped the `App` component in `React.StrictMode` for highlighting potential issues during development.
    - Rendered the application to the root element.
    
    This update ensures that the application uses the defined global styles and adheres to best practices for React development.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import './App.css';
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
