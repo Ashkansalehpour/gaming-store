e9a6da6577f9052124d0dfe2ae03023fa323c224
Ashkan <ashkansalehpoor81@gmail.com>
2024-09-04 14:19:53 +0330

    feat(pages): add ProductList page component
    Created a `ProductList` page component in `src/pages/ProductList.js` to display a list of all products available in the application.
    
    Features:
    - Renders a list of products using the `ProductCard` component for each item.
    - Displays a heading "All Products" to introduce the product list.
    - Utilizes Bootstrap grid classes for responsive layout, organizing products into a multi-column format.
    
    This component provides an overview of all products, enabling users to browse and select items for more detailed views.

---
+++ src/pages/ProductList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import products from '../data/products';
+import ProductCard from '../components/ProductCard';
+
+function ProductList() {
+  return (
+    <div className="product-list">
+      <h2>All Products</h2>
+      <div className="row">
+        {products.map(product => (
+          <div key={product.id} className="col-md-4 mb-4">
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
